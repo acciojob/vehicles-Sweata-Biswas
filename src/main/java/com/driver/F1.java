b0bbb014b723dd9f50972d9ecd4c3bbab7492bc4
Sweata-Biswas <biswassweata22@gmail.com>
2022-12-17 13:38:42 +0530

    final

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -18,7 +18,8 @@ public class F1 extends Car {
          * speed 201-250: gear 5
          * speed more than 250: gear 6
          */
-        newSpeed = newSpeed + rate;
+
+        newSpeed += newSpeed + rate;
 
         if(newSpeed == 0) {
             stop();
