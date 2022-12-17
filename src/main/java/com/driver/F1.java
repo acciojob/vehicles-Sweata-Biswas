de3640a7966e4336dcf7a6034c86bff58daa6943
Sweata-Biswas <biswassweata22@gmail.com>
2022-12-17 14:44:16 +0530

    final code

--- src/main/java/com/driver/F1.java
+++ src/main/java/com/driver/F1.java
@@ -29,23 +29,23 @@ public class F1 extends Car {
 
             //Stop the car, set gear as 1
         }
-        //for all other cases, change the gear accordingly
+       else if(newSpeed>0 && newSpeed<=50){
+            changeGear(1);
+        }else if(newSpeed>=51 && newSpeed<=100){
+            changeGear(2);
+        }else if(newSpeed>=101 && newSpeed<=150){
+            changeGear(3);
+        }else if(newSpeed>=151 && newSpeed<=200){
+            changeGear(4);
+        }else if(newSpeed>=201 && newSpeed<=250){
+            changeGear(5);
+        }else if(newSpeed>=251){
+            changeGear(6);
+        }
 
         if(newSpeed > 0) {
-//            changeSpeed(newSpeed, getCurrentDirection());
-            if(newSpeed>0 && newSpeed<=50){
-                changeGear(1);
-            }else if(newSpeed>=51 && newSpeed<=100){
-                changeGear(2);
-            }else if(newSpeed>=101 && newSpeed<=150){
-                changeGear(3);
-            }else if(newSpeed>=151 && newSpeed<=200){
-                changeGear(4);
-            }else if(newSpeed>=201 && newSpeed<=250){
-                changeGear(5);
-            }else if(newSpeed>=251){
-                changeGear(6);
-            }
+           changeSpeed(newSpeed, getCurrentDirection());
+
 
         }
     }
